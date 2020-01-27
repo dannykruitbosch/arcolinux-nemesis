cf7d667c4d5e66c357ad0001c3fab251181fad35
Danny Kruitbosch <danny.kruitbosch@gmail.com>
2020-01-27 19:44:34 +0100

    update handling of backlight

--- Personal/scripts/xbacklightmon.sh
+++ Personal/scripts/xbacklightmon.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #use LC_NUMERIC if you are using an European LC, else printf will not work because it expects an comma instead of a decimal point
 LC_NUMERIC="en_US.UTF-8"
+INTERNAL_MONITOR=$(xrandr -q | grep "^eDP" | awk '{print $1}')
 
 #Exit with 1 if $DISPLAY env isn't set. Helps when using the start up script below
 [ -z "$DISPLAY" ] && exit 1;
@@ -17,8 +18,8 @@ luminance() {
     printf '%f\n' $new_brightness
 }
 
-xrandr --output eDP-1 --brightness "$(luminance)"
+xrandr --output $INTERNAL_MONITOR --brightness "$(luminance)"
 
 inotifywait -me modify --format '' "$path"/actual_brightness | while read; do
-    xrandr --output eDP-1 --brightness "$(luminance)"
+    xrandr --output $INTERNAL_MONITOR --brightness "$(luminance)"
 done
